a939e48cf1f5bc54ba581298a43049035c9665ec
urprakashgupta <urprakashgupta@gmail.com>
2025-03-05 15:42:57 +0530

    add user authentication and blog routes; configure Prisma with PostgreSQL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,86 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-const app = new Hono()
+app.post("/api/v1/user/signup", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      "jwt_secret"
+    ); //this is hardcoded jwt secret you can store it in wrangler
+    return c.text(jwt);
+  } catch (error) {
+    c.status(411);
+    return c.text("Something went wrong");
+  }
+});
 
-export default app
+app.post("api/v1/user/signin", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+    if (!user) {
+      c.status(403);
+      return c.json({
+        message: "Incorrect credidenatals",
+      });
+    }
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      "jwt_secret"
+    ); //this is hardcoded jwt secret you can store it in wrangler
+    return c.text(jwt);
+  } catch (error) {
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get blog route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+export default app;
